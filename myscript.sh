f8226425445615ddc92930a6db3c4f3b5d88aeed
Ashutosh_K_Sah <64386187+ashu-cybertron@users.noreply.github.com>
2021-06-06 16:28:09 +0530

    Add files via upload

--- myscript.sh
+++ myscript.sh
@@ -1,4 +1,2 @@
-#!/bin/bash 
-
 nohup java -DJENKINS_HOME=/var/lib/jenkins -jar /usr/lib/jenkins/jenkins.war &
-/apache-tomcat-9.0.46/bin/catalina.sh jpda run
+/apache-tomcat-9.0.46/bin/catalina.sh jpda run && /usr/sbin/init
